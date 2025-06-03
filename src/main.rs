e4597262eb0fb9fa96f4ad4d59f20e57a77dc070
Zack Jensen <zack.j.jensen@gmail.com>
2025-06-03 16:35:13 -0600

    module lib updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use pokersim::cards::dealer;
+use pokersim::cards::dealer::Dealer;
+use pokersim::cards::card_enums::Card;
+
 
 fn main() {
-    let dealer = dealer::Dealer();
+    let mut dealer = Dealer::new();
+    dealer.shuffle();
+    let v = &dealer.deck;
+    for card_int in v {
+        println!("{}", Card::card_from_int(card_int))
+    }
 }

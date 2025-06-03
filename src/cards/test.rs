b7a476e133f8798b65219767411a1380d83ca7c9
Zack Jensen <zack.j.jensen@gmail.com>
2025-06-03 11:16:40 -0600

    removing unused file

--- src/cards/test.rs
+++
@@ -1,9 +0,0 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-
-pub fn shuffle() -> Vec<i32> {
-    let mut deck: Vec<i32> = (1..=52).collect();
-    let mut rng = thread_rng();
-    deck.shuffle(&mut rng);
-    deck
-}

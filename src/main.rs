41d05eb5a0a2064bfffcc30ed2ffb3f656620e59
Zack Jensen <zack.j.jensen@gmail.com>
2025-06-04 13:00:08 -0600

    initial holdem game

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use pokersim::cards::dealer::Dealer;
-use pokersim::cards::card_enums::Card;
+mod cards;
 
 
 fn main() {
-    let mut dealer = Dealer::new();
+    let mut dealer = cards::dealer::Dealer::new();
     dealer.shuffle();
     let v = &dealer.deck;
     for card_int in v {
-        println!("{}", Card::card_from_int(card_int))
+        println!("{}", cards::card_enums::Card::card_from_int(card_int))
     }
 }

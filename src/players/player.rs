62b07b8775f5d09851106b8b318dc06d4fe3eb80
Zack Jensen <zack.j.jensen@gmail.com>
2025-06-04 10:37:26 -0600

    initial holdem player with simple strategy

--- src/players/player.rs
+++ src/players/player.rs
@@ -15,40 +15,50 @@ impl Player {
 pub trait HoldemPlayer {
     fn recieve_cards(&mut self, cards: [u8; 2]) -> ();
     fn show(&self) -> [u8; 2];
-    // fn best_hand(&self, shared_cards: &Vec<u8>) -> &Vec<u8>;
-    fn play(&self, shared_cards: &Vec<u8>) -> Option<i32>;
-    fn bet(&self, shared_cards: &Vec<u8>) -> i32;
-    fn fold(&self, shared_cards: &Vec<u8>) -> ();
+    fn best_hand(&self, shared_cards: &Vec<u8>) -> [u8; 5];
+    fn play(&mut self, shared_cards: &Vec<u8>) -> Option<u32>;
+    fn bet(&mut self, shared_cards: &Vec<u8>) -> u32;
+    fn fold(&mut self) -> ();
 }
 
 impl HoldemPlayer for Player {
     fn recieve_cards(&mut self, cards: [u8; 2]) {
         self.hand = Some(cards);
     }
-    fn bet(&self, shared_cards: &Vec<u8>) -> i32 {
-        return 1
+    fn bet(&mut self, _shared_cards: &Vec<u8>) -> u32 {
+        // bet of 0 == check
+        if self.chips > 1 {
+            self.chips -= 1;
+            1
+        } else {
+            0
+        }
     }
     fn show(&self) -> [u8; 2] {
         match self.hand {
-            Some(hand) => println!("{}, {}", hand[0], hand[1]),
-            None => println!("No Cards"),
+            Some(hand) => return hand,
+            None => panic!("No Cards"),
         }
-        return [1, 2];
         
     }
-    fn play(&self, shared_cards: &Vec<u8>) -> Option<i32> {
+    fn play(&mut self, shared_cards: &Vec<u8>) -> Option<u32> {
+        if self.hand.is_none() {panic!("Player {} can't play, player has no cards", self.name)}
         if shared_cards.len() == 5 {
+            self.fold();
             return None
         } else {
-            return Some(1)
+            return Some(self.bet(shared_cards))
         }
     }
-    fn fold(&self, shared_cards: &Vec<u8>) -> () {
-        
+    fn fold(&mut self) -> () {
+        self.hand = None;
     }
-    // fn best_hand(&self, shared_cards: &Vec<u8>) -> &Vec<u8> {
-    //     
-    // }
+    fn best_hand(&self, shared_cards: &Vec<u8>) -> [u8; 5] {
+        match self.hand {
+            Some(hand) => return [hand[0], hand[1], shared_cards[0], shared_cards[1], shared_cards[2]],
+            None => panic!("Player {} has no cards", self.name)
+        }
+    }
 }
 
 
@@ -60,6 +70,25 @@ pub mod tests {
         let mut player = Player::new("test".to_string(), 10);
         let cards: [u8; 2] = [10, 15];
         player.recieve_cards(cards)
+    }
+
+    pub fn test_play() {
+        let mut player = Player::new("test".to_string(), 10);
+        let cards: [u8; 2] = [20, 30];
+        player.recieve_cards(cards);
+
+        let mut shared_cards: Vec<u8> = [2, 3, 4].to_vec();
+        let _play1 = player.play(&shared_cards);
+        assert_eq!(player.chips, 9);
+
+        shared_cards.push(5);
+        let _play2 = player.play(&shared_cards);
+        assert_eq!(player.chips, 8);
+
+        shared_cards.push(6);
+        let _play3 = player.play(&shared_cards);
+        assert_eq!(player.chips, 8);
+        assert!(player.hand.is_none());
     }
 
 }
